a4fdf5522ab7d6a019a99df143424f40a5f93bde
gwilcox <gwilcox@192.168.0.102>
2016-08-19 10:38:29 -0600

    minor value change

--- map/src/main/java/org/infinispan/tutorial/simple/map/InfinispanMap.java
+++ map/src/main/java/org/infinispan/tutorial/simple/map/InfinispanMap.java
@@ -11,9 +11,9 @@ public class InfinispanMap {
       // Obtain the default cache
       Cache<String, String> cache = cacheManager.getCache();
       // Store a value
-      cache.put("key", "value");
+      cache.put("key1", "value1");
       // Retrieve the value and print it out
-      System.out.printf("key = %s\n", cache.get("key"));
+      System.out.printf("key1 = %s\n", cache.get("key1"));
       // Stop the cache manager and release all resources
       cacheManager.stop();
    }
